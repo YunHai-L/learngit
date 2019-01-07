6ce4c7564482eaf3174f0888b8958680280141c1
yunhai <yunhaili.9527@gmail.com>
2019-01-07 17:56:07 +0800

    create helloIf.java

---
+++ helloIf.java
@@ -0,0 +1,7 @@
+class helloIf{
+	public static void main(String[] args){
+		if('a' == 97){
+			System.out.println("a = 97");
+		}
+	}
+}
--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,7 @@ class helloWorld{
 	public static void main(String[] args){
 		System.out.println("hello Git!");
 		System.out.println("hello Git! I will commit the update");
+		System.our.println("hello helloIf");
 	}
 
 }
