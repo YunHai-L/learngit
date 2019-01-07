2cf0df4b4a89c81f17e4e3154170cb487bcb1bbf
yunhai <yunhaili.9527@gmail.com>
2019-01-07 19:49:50 +0800

    add github

--- helloWorld.java
+++ helloWorld.java
@@ -2,7 +2,8 @@ class helloWorld{
 	public static void main(String[] args){
 		System.out.println("hello Git!");
 		System.out.println("hello Git! I will commit the update");
-		System.our.println("hello helloIf");
+		System.out.println("hello helloIf");
+		System.out.println("hello github");
 	}
 
 }
